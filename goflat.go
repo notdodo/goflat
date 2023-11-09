052aafe9baca5d41ab175cb1236b8c4bb8d11350
Edoardo Rosa <6991986+notdodo@users.noreply.github.com>
2023-11-09 21:02:00 +0100

    enh: refactoring with correct indexes

--- goflat.go
+++ goflat.go
@@ -2,92 +2,159 @@ package goflat
 
 import (
 	"errors"
-	"strconv"
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
 
 	json "github.com/ohler55/ojg/oj"
 )
 
 var ErrInvalidType = errors.New("not a valid JSON input")
 
-// Convert a JSON struct to a flat Map
-func FlatStruct(str any, prefix, separator string) (flattenMap map[string]interface{}, err error) {
-	jsonBytes, err := json.Marshal(str)
-	if err != nil {
-		return nil, err
-	}
-
-	flattenStr, err := Flat(string(jsonBytes), prefix, separator)
-	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal([]byte(flattenStr), &flattenMap)
-	return flattenMap, err
+// FlattenerConfig holds configuration options for flattening.
+type FlattenerConfig struct {
+	Prefix    string
+	Separator string
+	OmitEmpty bool
+	OmitNil   bool
+	SortKeys  bool
 }
 
-// Convert a JSON string to a flat JSON string
-// Only accepted inputs are: JSON objects or array of JSON objects
-func Flat(jsonStr, prefix, separator string) (string, error) {
-	unflattenInterface, err := json.ParseString(jsonStr)
-	if err != nil {
-		return "", err
+// DefaultFlattenerConfig returns a FlattenerConfig with default values.
+func DefaultFlattenerConfig() FlattenerConfig {
+	return FlattenerConfig{
+		Prefix:    "",
+		Separator: ".",
+		OmitEmpty: true,
+		OmitNil:   true,
+		SortKeys:  true,
+	}
+}
+
+func FlatStruct(input interface{}, config ...FlattenerConfig) map[string]interface{} {
+	cfg := DefaultFlattenerConfig()
+	if len(config) > 0 {
+		cfg = config[0]
 	}
 
-	switch v := unflattenInterface.(type) {
-	case []interface{}: // [{"a": 1}]
-		var flattenArrayMap []map[string]interface{}
-		for _, element := range v {
-			flattenArrayMap = append(flattenArrayMap, flattenMap(element.(map[string]interface{}), prefix, separator))
+	result := make(map[string]interface{})
+	flattenFields(reflect.ValueOf(input), cfg.Prefix, result, cfg)
+	if cfg.SortKeys {
+		keys := make(map[string]struct{})
+		for key := range result {
+			keys[cfg.Prefix+key] = struct{}{}
 		}
-		return json.JSON(flattenArrayMap), nil
-	case map[string]interface{}: // {"a": 1}
-		flattenArrayMap := flattenMap(v, prefix, separator)
-		return json.JSON(flattenArrayMap), nil
-	default:
+		return sortKeysAndReturnResult(result, keys)
+	}
+	return result
+}
+
+func FlatJSON(jsonStr string, config ...FlattenerConfig) (string, error) {
+	cfg := DefaultFlattenerConfig()
+	if len(config) > 0 {
+		cfg = config[0]
+	}
+
+	var data interface{}
+	err := json.Unmarshal([]byte(jsonStr), &data)
+	if err != nil {
 		return "", ErrInvalidType
 	}
+
+	flattenedMap := make(map[string]interface{})
+	flatten(cfg.Separator, cfg.Prefix, data, flattenedMap, cfg)
+	if cfg.SortKeys {
+		keys := make(map[string]struct{})
+		for key := range flattenedMap {
+			keys[cfg.Prefix+key] = struct{}{}
+		}
+		flattenedMap = sortKeysAndReturnResult(flattenedMap, keys)
+	}
+
+	flattenedJSON, err := json.Marshal(flattenedMap)
+	if err != nil {
+		return "", ErrInvalidType
+	}
+	return string(flattenedJSON), nil
 }
 
-func flatten(unflat interface{}, prefix, separator string, top bool) (flatMap map[string]interface{}) {
-	flatMap = make(map[string]interface{})
+func sortKeysAndReturnResult(result map[string]interface{}, keys map[string]struct{}) map[string]interface{} {
+	sortedResult := make(map[string]interface{})
+	sortedKeys := make([]string, 0, len(keys))
+	for key := range keys {
+		sortedKeys = append(sortedKeys, key)
+	}
+	sort.Strings(sortedKeys)
+	for _, key := range sortedKeys {
+		sortedResult[key] = result[key]
+	}
+	return sortedResult
+}
 
-	switch unv := unflat.(type) {
+func flatten(separator, prefix string, value interface{}, result map[string]interface{}, config FlattenerConfig) {
+	switch v := value.(type) {
 	case map[string]interface{}:
-		for key, value := range unv {
-			for k, v := range flatten(value, key, separator, false) {
-				newKey := prefix
-				if top {
-					newKey += k
-				} else {
-					newKey += separator + k
-				}
-				flatMap[newKey] = v
+		for key, val := range v {
+			fullKey := key
+			if prefix != "" {
+				fullKey = prefix + config.Separator + key
 			}
+			flatten(config.Separator, fullKey, val, result, config)
 		}
 	case []interface{}:
-		for i, val := range unv {
-			for k, v := range flatten(val, prefix, separator, false) {
-				flatMap[k+separator+strconv.Itoa(i)] = v
+		flattenArray(prefix, v, result, config)
+	default:
+		if !(config.OmitEmpty && isEmptyValue(reflect.ValueOf(v))) && !(config.OmitNil && isNilValue(reflect.ValueOf(v))) {
+			result[prefix] = v
+		}
+	}
+}
+
+func flattenArray(prefix string, arr []interface{}, result map[string]interface{}, config FlattenerConfig) {
+	for i, v := range arr {
+		fullKey := fmt.Sprintf("%s%s%s%d", config.Prefix, prefix, config.Separator, i)
+		if strings.Index(fullKey, config.Separator) == 0+len(config.Prefix) {
+			fullKey = fullKey[1:]
+		}
+		flatten("", fullKey, v, result, config)
+	}
+}
+
+func flattenFields(val reflect.Value, prefix string, result map[string]interface{}, config FlattenerConfig) {
+	typ := val.Type()
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldName := typ.Field(i).Name
+		fieldValue := field.Interface()
+
+		if !(config.OmitEmpty && isEmptyValue(field)) && !(config.OmitNil && isNilValue(field)) {
+			if field.Kind() == reflect.Struct {
+				flattenFields(field, prefix+fieldName+config.Separator, result, config)
+			} else if field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
+				flattenArrayField(prefix, fieldName, field, result, config)
+			} else {
+				result[prefix+fieldName] = fieldValue
 			}
 		}
-	default:
-		flatMap[prefix] = unflat
 	}
-
-	return
 }
 
-//nolint:unused
-func flattenArray(unflattenArrayMap []map[string]interface{}, prefix, separator string) (string, error) {
-	var outputArrayMap []map[string]interface{}
-
-	for _, unflattenMap := range unflattenArrayMap {
-		flatMap := flatten(unflattenMap, prefix, separator, true)
-		outputArrayMap = append(outputArrayMap, flatMap)
+func flattenArrayField(prefix, fieldName string, field reflect.Value, result map[string]interface{}, config FlattenerConfig) {
+	for i := 0; i < field.Len(); i++ {
+		item := field.Index(i).Interface()
+		key := fmt.Sprintf("%s%s%d", prefix+fieldName+config.Separator, config.Separator, i)
+		if !(config.OmitEmpty && isEmptyValue(reflect.ValueOf(item))) && !(config.OmitNil && isNilValue(reflect.ValueOf(item))) {
+			result[key] = item
+		}
 	}
-
-	return json.JSON(&outputArrayMap), nil
 }
 
-func flattenMap(unflattenMap map[string]interface{}, prefix, separator string) map[string]interface{} {
-	return flatten(unflattenMap, prefix, separator, true)
+func isEmptyValue(field reflect.Value) bool {
+	zero := reflect.Zero(field.Type())
+	return reflect.DeepEqual(field.Interface(), zero.Interface())
+}
+
+func isNilValue(field reflect.Value) bool {
+	return field.Kind() == reflect.Ptr && field.IsNil()
 }
